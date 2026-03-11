66bfe87f905c8af8f5d093c684540f36a6904fe5
OriSharaby <orihi232@gmail.com>
2026-03-11 14:51:41 +0200

    add protected route for chat page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,6 +3,7 @@ import { ToastContainer } from "react-toastify";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ChatPage from "./pages/ChatPage";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
 
 
 export default function App() {
@@ -13,8 +14,13 @@ export default function App() {
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-
+        <Route path="/chat"
+          element={
+            <ProtectedRoute>
+              <ChatPage />
+            </ProtectedRoute>
+          }
+        />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </>
