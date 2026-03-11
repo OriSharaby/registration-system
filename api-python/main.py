25127ea94b5c68e1e94267ad9bbe8216f5c3bd0c
OriSharaby <orihi232@gmail.com>
2026-03-11 23:13:23 +0200

    update CORS settings, enhance login response with user data, and clean up health route

--- api-python/main.py
+++ api-python/main.py
@@ -11,6 +11,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        "https://qo7jbrc-anonymous-8081.exp.direct"
     ],
     allow_credentials=True,
     allow_methods=["*"],
